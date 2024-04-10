c9372259edc61ace2aeeb75955e95a6400a5dad7
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-04-10 11:29:53 +0700

    Update page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+        <div className="flex flex-col gap-2 p-5 max-w-xs bg-slate-800 w-full rounded-lg">
+            <div className="text-center my-4">
+                <h1>Strings</h1>
+            </div>
+            <div>
+                <Link href="/signin" className="bg-slate-900 my-4 p-3 rounded-lg block ">Sign In</Link>
+            </div>
+            <div>
+                <Link href="/signup" className="bg-slate-900 my-4 p-3 rounded-lg block ">Sign Up</Link>
+            </div>
+        </div>
+    </main>
+  );
+}
