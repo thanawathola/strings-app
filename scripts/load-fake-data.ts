35c9c906bd7b280e53a83eea96e9e51cdf6ca5c7
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-04-16 14:05:24 +0700

    Update load-fake-data.ts

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,5 +1,23 @@
-function loadFakeData (numUsers: number = 10 ) {
+import { loadEnvConfig } from '@next/env';
+import { Client } from "pg";
+
+
+const projectDir =process.cwd();
+loadEnvConfig(projectDir);
+
+async function loadFakeData (numUsers: number = 10 ) {
     console.log(`executing load fake data .generating ${numUsers} users`);
+
+    const client = new Client({
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_NAME,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT!),
+    });
+
+    const res = await client.query("select 1")
+    console.log(res);
 }
 
 loadFakeData ();
