f75fd374ca9da0ce523ecf8ff669a24c5f603260
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-04-23 14:16:22 +0700

    postgres url

--- db.ts
+++ db.ts
@@ -1,24 +1,33 @@
 import { Client, QueryResult } from "pg";
 import { loadEnvConfig } from "@next/env";
 
-const projectDir =process.cwd();
+const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 export async function getClient(): Promise<Client> {
+  if (process.env.POSTGRES_URL) {
     const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
+      connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     });
     return client;
+  }
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
+  return client;
 }
 
-export async function sql(sql : string, values?: Array <any>): Promise<QueryResult<any>> {
-    const client = await getClient();
-    await client.connect();
-    const res = await client.query(sql, values);
-    await client.end();
-    return res;
+export async function sql(
+  sql: string,
+  values?: Array<any>
+): Promise<QueryResult<any>> {
+  const client = await getClient();
+  await client.connect();
+  const res = await client.query(sql, values);
+  await client.end();
+  return res;
 }
