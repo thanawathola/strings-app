3d4e120978b75b1fd5eec86258d6eef1a8c1486f
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-04-25 13:45:29 +0700

    header. user component

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useSWR from "swr";
+import User from "../components/user";
 
 export default function Header() {
   const { data, error, isLoading } = useSWR("/api/users/profile");
@@ -9,5 +10,14 @@ export default function Header() {
 
   console.log(data);
 
-  return <header>{data?.data?.username}</header>;
+  return (
+  <header className="flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center">
+    <div>
+      <h1 className="font-mono text-lg">Strings</h1>
+    </div>
+    <div>
+      <User user ={data?.data} href ="account"/>
+    </div>
+  </header>
+  );
 }
---
+++ app/components/user.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+//https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/923.jpg
+
+function User({ user, href }: { user: UserI; href?: string }) {
+  return (
+    <div>
+      <Link
+        href={`/${href || user.username}`}
+        className="flex flex-row items-center"
+      >
+        <div>
+          {user.avatar && (
+            <Image
+              src={user.avatar}
+              width={50}
+              height={50}
+              alt={user.username}
+              className="rounded-full mr-3"
+            />
+          )}
+          {!user.avatar && (
+            <div
+              style={{ width: 50, height: 50 }}
+              className="bg-slate-600 rounded-full mr-3"
+            ></div>
+          )}
+        </div>
+        <div>{user.username}</div>
+      </Link>
+    </div>
+  );
+}
+
+export default User;
