6361c42e64535c377d1a47082170dd3d7589a4a4
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-04-25 14:11:21 +0700

    style navbar and footer

--- app/components/user.tsx
+++ app/components/user.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-//https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/923.jpg
+
 
 function User({ user, href }: { user: UserI; href?: string }) {
   return (
