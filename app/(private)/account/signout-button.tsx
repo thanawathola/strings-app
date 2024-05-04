010f11aebc7b61043f7f0ce415d96e5d581517cf
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-05-04 12:00:07 +0700

    account page

---
+++ app/(private)/account/signout-button.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/navigation";
+
+
+export default function SignOutButton() {
+  const router = useRouter(); //use this to go back sign in page
+
+  async function handleSignOut() {
+    const res = await fetch("api/logout");
+
+    if (res.ok) {
+      router.push("/signin");
+    }
+  }
+
+  return (
+    <button
+      onClick={handleSignOut}
+      className="text-green-400 underline p-2 rounded-lg my-5"
+    >
+      Sign Out
+    </button>
+  );
+}
