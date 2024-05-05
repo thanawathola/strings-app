96213f4d54b4057387a4a534d1eca91037f76971
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-05-05 15:23:24 +0700

    wip: search bar

---
+++ app/(private)/search-bar.tsx
@@ -0,0 +1,30 @@
+import { ChangeEvent, useState } from "react";
+
+export default function SearchBar() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  async function fetchSearchResults(searchText: string) {
+    const res = await fetch("/api/search?q=" + searchText);
+    if (res.ok) {
+      const json = await res.json();
+      console.log(json);
+      setSearchResults(json.data);
+    }
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log("change", e.target.value);
+    fetchSearchResults(e.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        onChange={handleChange}
+        type="text"
+        className="p-2 rounded-lg bg-slate-700 my-2"
+        placeholder="Search"
+      />
+    </div>
+  );
+}
