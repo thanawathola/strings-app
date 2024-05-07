4f72281d8d21da2239c06d3a96f6e24f69ed4d74
thanawathola <151997870+thanawathola@users.noreply.github.com>
2024-05-07 12:20:04 +0700

    flag misinformation endpoint. authorization logic

---
+++ app/api/admin/flag-misinformation/[id]/route.ts
@@ -0,0 +1,18 @@
+import { sql } from "@/db";
+import { authorizeAdmin } from "@/util/auth";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  return authorizeAdmin(async () => {
+    const { id } = params;
+    console.log(`flagging ${id} as misinformation`);
+    await sql(
+      "update posts set is_misinformation = true , is_misinformation_flagged_at =now() where id = $1",
+      [id]
+    );
+    return NextResponse.json({ msg: "flagged as misinformation " });
+  });
+}
